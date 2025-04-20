ee13354c64b6dbffad82b6187990ba8d7e0d797b
atsuikeda <ike.ke.no.ke@gmail.com>
2025-04-20 17:05:31 +0900

    打鍵数カウント、打鍵に応じて表示画像切り替え

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <h1>DAKEN！</h1>
-    <p>打鍵数: {{ dakenCount }}</p>
+    <h1>伝えたい</h1>
+    <p>{{ keydownCount }}えっほ</p>
     <div>
-      <p>打鍵してね</p>
+      <img :src="isRightFoot ? rightImg : leftImg" alt="" />
     </div>
   </div>
 </template>
@@ -11,33 +11,24 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-const dakenCount = ref(0)
+const keydownCount = ref(0)
+const isRightFoot = ref(true)
 
-const isExtension = typeof chrome !== 'undefined' && chrome.storage
+const rightImg = '/right.jpg'
+const leftImg = '/left.jpg'
+
+const handleKeydown = () => {
+  keydownCount.value++
+  localStorage.setItem('keydownCount', keydownCount.value)
+
+  isRightFoot.value = !isRightFoot.value
+}
 
 onMounted(() => {
-  if (isExtension) {
-    chrome.storage.local.get(['dakenCount'], (result) => {
-      console.log('取得した値:', result)
-      dakenCount.value = result.dakenCount || 0
-    })
+  window.addEventListener('keydown', handleKeydown)
 
-    chrome.storage.onChanged.addListener((changes) => {
-      console.log('変更を検出:', changes)
-      if (changes.dakenCount) {
-        dakenCount.value = changes.dakenCount.newValue
-      }
-    })
-  } else {
-    const local = localStorage.getItem('dakenCount')
-    dakenCount.value = local ? parseInt(local) : 0
-
-    window.addEventListener('keydown', () => {
-      dakenCount.value++
-      localStorage.setItem('dakenCount', dakenCount.value)
-      console.log('ローカルキー押した！カウント:', dakenCount.value)
-    })
-  }
+  const local = localStorage.getItem('keydownCount')
+  keydownCount.value = local ? parseInt(local) : 0
 })
 </script>
 
